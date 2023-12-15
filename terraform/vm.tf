bd0c71a75a10ce2d495f34fab00ca980cb3b838f
efoppiano <efoppiano@fi.uba.ar>
2023-12-15 20:41:17 -0300

    Reduce disk size of manager VM

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "vms" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      size = count.index == 0 ? 50 : 10
+      size = count.index == 0 ? 30 : 10
     }
   }
 
