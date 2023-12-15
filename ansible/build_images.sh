a06b95d23f6dda7cf137142ea46ab2bb99b2bca8
efoppiano <efoppiano@fi.uba.ar>
2023-12-15 20:52:20 -0300

    Remove debug logs

--- ansible/build_images.sh
+++ ansible/build_images.sh
@@ -3,9 +3,6 @@ OLD_DIR="$(pwd)"
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit
 
-echo first: "$1"
-echo second: "$2"
-
 ansible-playbook -i hosts --private-key ../key.pem -u ubuntu\
  -e "src_dir=\"$1\" build_command=\"$2\""\
  build_images.yaml
